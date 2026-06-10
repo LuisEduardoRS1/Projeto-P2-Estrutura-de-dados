7634983018487a12ebab40602cfb60dbfefd211f
LuisEduardo <luiseduardoribeiro49@gmail.com>
2026-06-10 00:34:44 -0300

    implementação da classe Fila

---
+++ src/Fila.java
@@ -0,0 +1,57 @@
+public class Fila<T> {
+    private T info;
+    private No<T> primeiro;
+    private No<T> ultimo;
+    private int ocupacao = 0;
+
+    public boolean estaVazio(){
+        return primeiro == null;
+    }
+
+    public void enfileira(T info){
+        No<T> novo = new No<T>(info);
+        if (estaVazio()) {
+            primeiro = novo;
+        }else{
+            ultimo.setProximo(novo);
+        }
+        ultimo = novo;
+        ocupacao++;
+    }
+
+    public T desenfileira()throws Exception{
+        if (estaVazio()) {
+            throw new Exception("Fila vazia");
+        }
+        T aux = primeiro.getInfo();
+        primeiro = primeiro.getProximo();
+        if(primeiro == null){
+            ultimo = null;
+        }
+        ocupacao--;
+        return aux;
+    }
+
+    public T primeiro(){
+        return primeiro.getInfo();
+    }
+
+    public int tamanho(){
+        return ocupacao;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        if (estaVazio()) {
+            return "Lista vazia";
+        }
+        No<T> runner = primeiro;
+        while (runner != null) {
+            sb.append("[" + runner.getInfo() + "] -> ");
+            runner = runner.getProximo();
+        }
+        sb.append("//");
+        return new String(sb);
+    }
+}
